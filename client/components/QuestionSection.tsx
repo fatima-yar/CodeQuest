cd22ee00d2283aff8a3f6b4101928b847cd61b2a
Fatima Yarhamidi <fatima.yarhamidi@gmail.com>
2024-07-25 17:09:20 +1200

    add celebration

--- client/components/QuestionSection.tsx
+++ client/components/QuestionSection.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { Question } from '../../models/questions'
 import { Answers } from './Answers'
+import confetti from 'canvas-confetti'
 
 interface Props {
   questions: Question[]
@@ -10,8 +11,26 @@ export default function QuestionSection({ questions }: Props) {
   const [currentQuestion, setCurrentQuestion] = useState(0)
   const questionObj = questions[currentQuestion]
 
+  // const handleNextQuestion = () => {
+  //   setCurrentQuestion((currentQuestion + 1) % questions.length)
+  // }
   const handleNextQuestion = () => {
-    setCurrentQuestion((currentQuestion + 1) % questions.length)
+    setCurrentQuestion((prevIndex) => {
+      const newIndex = prevIndex + 1
+
+      // Check if the new index is 10 and trigger the celebration
+      if (newIndex === 10) {
+        // Trigger confetti celebration
+        confetti({
+          particleCount: 200,
+          spread: 70,
+          origin: { y: 0.6 },
+        })
+      }
+
+      // Return the new index
+      return newIndex
+    })
   }
 
   return (
